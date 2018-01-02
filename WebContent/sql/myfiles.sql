8307355cc35cfe36c5e45a9a814782245a523fba
rentutu <rentutu@shaojia>
2018-01-02 11:24:57 +0800

    修改myfile.sql的顺序

--- WebContent/sql/myfiles.sql
+++ WebContent/sql/myfiles.sql
@@ -15,67 +15,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `man_privilege`
---
 
-DROP TABLE IF EXISTS `man_privilege`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `man_privilege` (
-  `id` int(11) NOT NULL,
-  `upload_pvl` int(11) DEFAULT '0',
-  `update_pvl` int(11) DEFAULT '0',
-  `grant_pvl` int(11) DEFAULT '0',
-  `all_files_pvl` int(11) DEFAULT '0',
-  `main_pvl` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`),
-  CONSTRAINT `manger_privilege` FOREIGN KEY (`id`) REFERENCES `myfiles_manager` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `man_privilege`
---
-
-LOCK TABLES `man_privilege` WRITE;
-/*!40000 ALTER TABLE `man_privilege` DISABLE KEYS */;
-<<<<<<< HEAD
-INSERT INTO `man_privilege` VALUES (5,0,0,0,0,0),(6,0,0,0,0,0),(7,0,0,0,0,1);
-/*!40000 ALTER TABLE `man_privilege` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `myfile`
---
-
-DROP TABLE IF EXISTS `myfile`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `myfile` (
-  `id` int(11) NOT NULL,
-  `file_name` varchar(30) DEFAULT NULL,
-  `save_path` varchar(30) DEFAULT NULL,
-  `owner_id` int(11) DEFAULT NULL,
-  `create_date` datetime DEFAULT NULL,
-  `last_modified_date` datetime DEFAULT NULL,
-  `last_modified_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `owner_id` (`owner_id`),
-  KEY `last_modified_id` (`last_modified_id`),
-  CONSTRAINT `myfile_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `myfiles_manager` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `myfile_ibfk_2` FOREIGN KEY (`last_modified_id`) REFERENCES `myfiles_manager` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `myfile`
---
-
-LOCK TABLES `myfile` WRITE;
-/*!40000 ALTER TABLE `myfile` DISABLE KEYS */;
-/*!40000 ALTER TABLE `myfile` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `myfiles_manager`
@@ -104,16 +44,31 @@ INSERT INTO `myfiles_manager` VALUES (5,'linshaojia','linshaojia',1),(6,'linlin1
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Table structure for table `man_privilege`
+--
 
--- Dump completed on 2017-09-27  4:16:28
-=======
+DROP TABLE IF EXISTS `man_privilege`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `man_privilege` (
+  `id` int(11) NOT NULL,
+  `upload_pvl` int(11) DEFAULT '0',
+  `update_pvl` int(11) DEFAULT '0',
+  `grant_pvl` int(11) DEFAULT '0',
+  `all_files_pvl` int(11) DEFAULT '0',
+  `main_pvl` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `manger_privilege` FOREIGN KEY (`id`) REFERENCES `myfiles_manager` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `man_privilege`
+--
+
+LOCK TABLES `man_privilege` WRITE;
+/*!40000 ALTER TABLE `man_privilege` DISABLE KEYS */;
 INSERT INTO `man_privilege` VALUES (5,0,0,0,0,0),(6,0,0,0,0,0),(7,1,1,1,1,1);
 /*!40000 ALTER TABLE `man_privilege` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -150,32 +105,6 @@ LOCK TABLES `myfile` WRITE;
 /*!40000 ALTER TABLE `myfile` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `myfiles_manager`
---
-
-DROP TABLE IF EXISTS `myfiles_manager`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `myfiles_manager` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `account` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `is_activited` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `myfiles_manager`
---
-
-LOCK TABLES `myfiles_manager` WRITE;
-/*!40000 ALTER TABLE `myfiles_manager` DISABLE KEYS */;
-INSERT INTO `myfiles_manager` VALUES (5,'linshaojia','linshaojia',1),(6,'linlin123','linlin123',0),(7,'adminmfm','sorrylinshaojia',1);
-/*!40000 ALTER TABLE `myfiles_manager` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -186,4 +115,3 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2017-09-30 16:51:55
->>>>>>> refs/heads/modalFix
